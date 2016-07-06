e8beaf26a1dfe9b75a5ea3ce807db40dd902426c
reflex-circle <reflex.circle@gmail.com>
2016-07-07 01:27:44 +0900

    Update sample_spec.rb

--- sample_spec.rb
+++ sample_spec.rb
@@ -35,6 +35,7 @@ describe "ログイン" do
     puts CGI.pretty(page.body)
     find('#global-new-tweet-button').click
     find('#tweet-box-global').set(Time.now)
+    find('.btn primary-btn tweet-action tweet-btn js-tweet-btn').click
   end
 =begin
   it "ツイートできること" do
