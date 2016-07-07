414ddee94c1f28fb161a33dc89efce0a6e7d86ff
reflex-circle <reflex.circle@gmail.com>
2016-07-08 00:10:44 +0900

    Update github-update-bio_spec.rb

--- github-update-bio_spec.rb
+++ github-update-bio_spec.rb
@@ -15,16 +15,30 @@ describe "gitHubにログインしてBioを更新する" do
   end
 
   it "ログインできること" do
+    # case1 - input
     fill_in 'login',    with: ENV['GITHUB_ID']
     fill_in 'password', with: ENV['GITHUB_PW']
     click_on 'Sign in'
+    #case1 - output
+    expect(page).to have_content 'Discover interesting projects and people to populate your personal news feed.'
+   
+    # case2 - input
     click_link '@reflex-circle'
     click_link 'Your profile'
-    puts CGI.pretty(page.body)
+    # case2 - output
+    expect(page).to have_title ENV['GITHUB_ID']
+   
+    # case3 - input 
     visit 'https://github.com/settings/profile'
-    fill_in 'user_profile_bio', with: Time.now.strftime("%Y-%m-%d %H:%M:%S") + ' by CircleCI'
+    # case3 - output
+    expect(page).to have_http_status(:success)
     
+    # case4 - input
+    @outtime = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    fill_in 'user_profile_bio', with: outtime + ' by CircleCI'
     click_on 'Update profile'
+    visit https://github.com/reflex-circle
+    # case4 - output
+    expect(page).to have_content @outtime
   end
-
 end
